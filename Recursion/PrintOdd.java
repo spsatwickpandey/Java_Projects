4aef5c6390f28b4d61e02db30a35ab8ef309f0ca
Satwick <satwickpandey@gmail.com>
2025-04-03 11:20:24 +0530

    PrintOdd

--- Recursion/PrintOdd.java
+++ Recursion/PrintOdd.java
@@ -3,5 +3,19 @@ import java.util.*;
 public class PrintOdd {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Range: ");
+        int n = sc.nextInt();
+
+        oddPrint(n);
+    }
+    public static void oddPrint(int n){
+        if(n==0){
+            return;
+        }
+        oddPrint(n-1);
+        if(n%2!=0){
+            System.out.println(n);
+        }
     }
 }
